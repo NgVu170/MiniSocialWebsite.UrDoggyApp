63bf481b5b4828b289d193e3ec46c0e0430b91f4
Alex <vudao210595@gmail.com>
2025-08-18 14:42:12 +0700

    Initial

--- UrDoggy.Website/UrDoggy.Services/obj/Debug/net8.0/UrDoggy.Services.AssemblyInfo.cs
+++ UrDoggy.Website/UrDoggy.Services/obj/Debug/net8.0/UrDoggy.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrDoggy.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4e1054d6b8c823c9a50ba5be839ce7b9abd76b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852d245cc99d1ab7b0f43db2401cd0d40922f1ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrDoggy.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrDoggy.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
