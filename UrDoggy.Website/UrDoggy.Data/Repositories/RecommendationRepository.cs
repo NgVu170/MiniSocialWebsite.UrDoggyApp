c821a3f714a984f990f3a5915dcd6b0f1024877f
Alex <vudao210595@gmail.com>
2025-09-25 20:15:15 +0700

    Alogrithm Logic

---
+++ UrDoggy.Website/UrDoggy.Data/Repositories/RecommendationRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrDoggy.Core.Models;
+
+namespace UrDoggy.Data.Repositories
+{
+    public class RecommendationRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public RecommendationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<float> RankCalculate(int currentUserId, Post Post)
+        {
+            float result = 0;
+            var relationship = _context.Friends.Where(r =>
+                (r.UserId == currentUserId && r.FriendId == Post.UserId) 
+                || (r.UserId == Post.UserId && r.FriendId == currentUserId))
+                .FirstOrDefault();
+
+            if (relationship != null)
+            {
+                if (relationship.Status == "Accepted")
+                {
+                    result += 5; // Bạn bè
+                }
+                else if (relationship.Status == "Pending")
+                {
+                    result += 2; // Đang chờ kết bạn
+                }
+                else if (relationship.Status == "Rejected")
+                {
+                    result = 0; // Bị từ chối
+                }
+                
+                return result;
+            } else
+            {
+                result += 1; // Không có quan hệ
+            }
+             return result += (Post.UpVotes - Post.DownVotes) * 0.01f; // Điểm từ lượt vote
+        }
+    }
+}
---
+++ UrDoggy.Website/UrDoggy.Services/Interfaces/IRecommendationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrDoggy.Core.Models;
+
+namespace UrDoggy.Services.Interfaces
+{
+    public interface IRecommendationService
+    {
+        Task<float> RankCalculate(int currentUserId, Post Post);
+
+    }
+}
