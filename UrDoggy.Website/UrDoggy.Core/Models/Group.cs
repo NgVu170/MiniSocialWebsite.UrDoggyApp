65c9acb5f8807274b7a58b94713fd10bc29dc269
Alex <vudao210595@gmail.com>
2025-09-27 15:55:53 +0700

    Create table Group & Group detail

---
+++ UrDoggy.Website/UrDoggy.Core/Models/Group.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrDoggy.Core.Models
+{
+    [Table ("Groups")]
+    public class Group
+    {
+        int Id { get; set; }
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public int OwnerId { get; set; }
+        public Status GroupStatus { get; set; } = Status.Active;
+
+        //Navigation
+        public User Owner { get; set; }
+    }
+
+    public enum Status
+    {
+        Active,
+        Warning,
+        Banned,
+        Deleted
+    }
+}
---
+++ UrDoggy.Website/UrDoggy.Core/Models/GroupDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrDoggy.Core.Models
+{
+    [Table("GroupDetails")]
+    public class GroupDetail
+    {
+        int GroupId { get; set; }
+        public int MemberIds { get; set; }
+        public GroupRole Roles { get; set; } = GroupRole.Member; // default role is Member
+        public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public Status MemberStatus { get; set; } = Status.Active; // default status is Active
+        int activtyScore { get; set; } = 0; // default activity score is 0
+        //navigation
+        [ForeignKey("GroupId")]
+        public Group Group { get; set; }
+    }
+
+    public enum GroupRole
+    {
+        Owner,
+        Admin,
+        Member
+    }
+}
