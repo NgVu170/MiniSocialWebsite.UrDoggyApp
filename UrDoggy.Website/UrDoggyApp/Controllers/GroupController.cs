ceb0a8c7b93ec4f35ed273b4f08daca367d364d2
Alex <vudao210595@gmail.com>
2025-11-01 21:33:33 +0700

    Refactor code

--- UrDoggy.Website/UrDoggyApp/Controllers/GroupController.cs
+++ UrDoggy.Website/UrDoggyApp/Controllers/GroupController.cs
@@ -1,22 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System.Security.Claims;
 using UrDoggy.Core.Models;
 using UrDoggy.Core.Models.GroupModels;
 using UrDoggy.Data;
-using UrDoggy.Services.Interfaces;
 using UrDoggy.Services.Interfaces.GroupServices;
-using UrDoggy.Services.Service;
-using UrDoggy.Services.Service.GroupServices;
-
 
 namespace UrDoggy.Website.Controllers
 {
     [Authorize]
-    [Route("[controller]/[action]/{groupId:int}")]
+    [Route("[controller]/[action]")]
     public class GroupController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -24,7 +18,6 @@ namespace UrDoggy.Website.Controllers
         private readonly IAdminGroupService _adminGroupService;
         private readonly IModeratorService _moderatorService;
         private int? userId;
-        protected int groupId;
 
         public GroupController(
             IGroupUserService groupUserService,
@@ -36,32 +29,30 @@ namespace UrDoggy.Website.Controllers
             _groupUserService = groupUserService;
             _adminGroupService = adminGroupService;
             _moderatorService = moderatorService;
-            //check login
-            CheckLogin();
         }
 
-        private IActionResult CheckLogin()
+        private bool CheckLogin()
         {
             userId = HttpContext.Session.GetInt32("UserId");
-            if (userId == null)
-            {
-                return RedirectToAction("Login", "Auth");
-            }
-            return null;
+            return userId != null;
         }
 
-
-        #region User
+        // ============= USER ZONE ==============
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
             var modGroups = await _context.GroupDetails
-            .Where(gd => gd.UserId == userId &&
-                    (gd.Role == Core.Models.GroupModels.GroupRole.Admin ||
-                     gd.Role == Core.Models.GroupModels.GroupRole.Moderator))
-            .Select(gd => gd.GroupId)
-            .ToHashSetAsync();
-            var userInGroups = await _context.GroupDetails.Where(gd => gd.UserId == userId).ToListAsync();
+                .Where(gd => gd.UserId == userId &&
+                        (gd.Role == GroupRole.Admin || gd.Role == GroupRole.Moderator))
+                .Select(gd => gd.GroupId)
+                .ToHashSetAsync();
+
+            var userInGroups = await _context.GroupDetails
+                .Where(gd => gd.UserId == userId)
+                .ToListAsync();
 
             ViewBag.UserInGroups = userInGroups;
             ViewBag.ModGroups = modGroups;
@@ -69,244 +60,173 @@ namespace UrDoggy.Website.Controllers
             var groups = await _groupUserService.GetAllGroup();
             return View(groups);
         }
+
         [HttpPost]
-        public async Task CreateGroup(FormCollection collection)
+        public async Task<IActionResult> CreateGroup(string GroupName, string Description, string Avatar, string CoverImage)
         {
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
             var newGroup = new Group
             {
-                GroupName = collection["GroupName"].ToString(),
-                Description = collection["Description"].ToString(),
-                Avatar = collection["Avatar"].ToString(),
-                CoverImage = collection["CoverImage"].ToString(),
+                GroupName = GroupName,
+                Description = Description,
+                Avatar = Avatar,
+                CoverImage = CoverImage,
                 OwnerId = userId.Value,
                 CreatedAt = DateTime.UtcNow,
                 UpdatedAt = DateTime.UtcNow,
                 GroupStatus = Status.Active
             };
             await _adminGroupService.CreateGroup(newGroup, userId.Value);
+            return RedirectToAction("Index");
         }
+
         [HttpPost]
-        public async Task LeaveGroup(int groupId)
+        public async Task<IActionResult> LeaveGroup(int groupId)
         {
-            int? userId = HttpContext.Session.GetInt32("UserId");
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
             await _groupUserService.LeaveGroup(userId.Value, groupId);
-            RedirectToAction("Index");
+            return RedirectToAction("Index");
         }
+
         [HttpPost]
-        public async Task JoinGroup(int groupId)
+        public async Task<IActionResult> JoinGroup(int groupId)
         {
-            int? userId = HttpContext.Session.GetInt32("UserId");
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
             await _groupUserService.JoinGroup(userId.Value, groupId);
-            RedirectToAction("Index");
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
-        public async Task<IActionResult> PostsInGroup(int groudpId)
+        public async Task<IActionResult> PostsInGroup(int groupId)
         {
-            userId = HttpContext.Session.GetInt32("UserId");
-            var posts = await _groupUserService.GetAllPost(groudpId);
-            var groupInformation = _context.Groups.FirstOrDefault(g => g.Id == groudpId);
-            var modList = _context.GroupDetails.Where(gd => gd.Role == GroupRole.Moderator).ToList();
-            var temp = _context.GroupDetails
-                .FirstOrDefault(gd => gd.GroupId == groudpId && gd.UserId == userId);
-            ViewBag.Role = temp.Role;
-            ViewBag.UserId = userId;
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
+            var posts = await _groupUserService.GetAllPost(groupId);
+            var groupInfo = await _context.Groups.Include(g => g.Owner).FirstOrDefaultAsync(g => g.Id == groupId);
+            var modList = await _context.GroupDetails.Include(gd => gd.User)
+                .Where(gd => gd.GroupId == groupId && gd.Role == GroupRole.Moderator)
+                .ToListAsync();
+
+            var userRole = await _groupUserService.getRole(userId.Value, groupId);
+
+            ViewBag.GroupInformation = groupInfo;
             ViewBag.ModList = modList;
-            ViewBag.GroupInformation = groupInformation;
+            ViewBag.Role = userRole;
+            ViewBag.UserId = userId;
+
             return View(posts);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> CreatePost()
+        // ============= GROUP MANAGEMENT ZONE ==============
+        private bool CheckPermission(int userId, int groupId)
         {
-            var group = await _context.Groups.FindAsync(groupId);
-            if (group == null)
-            {
-                return NotFound();
-            }
-
-            ViewBag.GroupId = groupId;
-            ViewBag.GroupName = group.GroupName;
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreatePost(int groupId, string content, List<IFormFile> MediaFiles)
-        {
-            CheckLogin();
-            var user = await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == userId);
-            if (user == null)
-                return Unauthorized();
-            List<Media> mediaItems = new List<Media>();
-            var rootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
-            var userFolder = Path.Combine(rootPath, $"user_{user.Id}");
-            if (!Directory.Exists(userFolder))
-                Directory.CreateDirectory(userFolder);
-
-            var newPost = new Post
-            {
-                Content = content,
-                CreatedAt = DateTime.UtcNow,
-                UserId = user.Id,
-                GroupId = groupId,
-            };
-            if (MediaFiles != null && MediaFiles.Any())
-            {
-                var postFolder = Path.Combine(userFolder, $"post_{newPost.Id}");
-                if (!Directory.Exists(postFolder))
-                    Directory.CreateDirectory(postFolder);
-
-                foreach (var file in MediaFiles)
-                {
-                    var extension = Path.GetExtension(file.FileName);
-                    var fileName = $"{Guid.NewGuid()}{extension}";
-                    var filePath = Path.Combine(postFolder, fileName);
-                    using (var stream = new FileStream(filePath, FileMode.Create))
-                    {
-                        await file.CopyToAsync(stream);
-                    }
-                    var mediaType = file.ContentType.StartsWith("video") ? "video" : "image";
-
-                    mediaItems.Add(new Media
-                    {
-                        PostId = newPost.Id,
-                        Path = $"/uploads/user_{user.Id}/post_{newPost.Id}/{fileName}",
-                        MediaType = mediaType,
-                        CreatedAt = DateTime.UtcNow
-                    });
-                }
-                await _groupUserService.CreatePost(newPost, mediaItems.Select(m => (m.Path, m.MediaType)));
-            }
-            return RedirectToAction("PostsInGroup", new { groudpId = groupId });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> DeletePostByAuthor(int postId, int groupId)
-        {
-            CheckLogin();
-            var post = await _context.Posts.AsNoTracking().FirstOrDefaultAsync(p => p.Id == postId);
-            if (post == null)
-                return NotFound();
-            if (post.UserId != userId)
-                return Forbid();
-            await _groupUserService.DeletePost(postId);
-            return RedirectToAction("PostsInGroup", new { groudpId = groupId });
-        }
-        [HttpPost]
-        public async Task<IActionResult> DeletePostByManagement(int postId, int modId)
-        {
-            CheckLogin();
-            var post = await _context.Posts.AsNoTracking().FirstOrDefaultAsync(p => p.Id == postId);
-            if (post == null)
-                return NotFound();
-            await _groupUserService.DeletePost(postId, modId);
-            return RedirectToAction("PostsInGroup", new { groudpId = groupId });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> ReportPost(int postId, int groupId, string reason)
-        {
-            CheckLogin();
-            var post = await _context.Posts.AsNoTracking().FirstOrDefaultAsync(p => p.Id == postId);
-            if (post == null)
-                return NotFound();
-            await _groupUserService.ReportPost(postId, userId.Value, reason);
-            return RedirectToAction("PostsInGroup", new { groudpId = groupId });
-        } //Bam vao goi form r nhap reason r gui len cho Admin
-        #endregion
-
-        #region Group managaement
-        private bool CheckPermision(int userId, int groupId)
-        {
-            var findUser = _context.GroupDetails
+            var userInGroup = _context.GroupDetails
                 .FirstOrDefault(gd => gd.GroupId == groupId && gd.UserId == userId);
-            if (findUser == null) return false;
 
-            if (findUser.Role == Core.Models.GroupModels.GroupRole.Admin ||
-                findUser.Role == Core.Models.GroupModels.GroupRole.Moderator)
-            {
-                return true;
-            }
-            else
-                return false;
+            return userInGroup != null && (userInGroup.Role == GroupRole.Admin || userInGroup.Role == GroupRole.Moderator);
         }
 
         [HttpGet]
         public async Task<IActionResult> GroupManagement(int groupId)
         {
-            if (int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
-            {
-                if (!CheckPermision(userId, groupId))
-                {
-                    return Forbid();
-                }
-                var group = await _groupUserService.getGroupById(groupId);
-                var pendingPostList = await _adminGroupService.GetAllPendingPost(groupId);
-                var memberList = await _groupUserService.GetAllMemberInGroup(groupId);
-                var reportList = await _moderatorService.GetAllReportPost(groupId);
-                var role = await _groupUserService.getRole(userId, groupId);
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
 
-                ViewBag.PendingPostList = pendingPostList;
-                ViewBag.MemberList = memberList;
-                ViewBag.ReportList = reportList;
-                ViewBag.Role = role;
-                return View(group);
-            }
-            else
-            {
-                return Unauthorized();
-            }
+            if (!CheckPermission(userId.Value, groupId))
+                return Forbid();
+
+            var group = await _groupUserService.getGroupById(groupId);
+            var pendingPostList = await _adminGroupService.GetAllPendingPost(groupId);
+            var memberList = await _groupUserService.GetAllMemberInGroup(groupId);
+            var reportList = await _moderatorService.GetAllReportPost(groupId);
+            var role = await _groupUserService.getRole(userId.Value, groupId);
+
+            ViewBag.PendingPostList = pendingPostList;
+            ViewBag.MemberList = memberList;
+            ViewBag.ReportList = reportList;
+            ViewBag.Role = role;
+
+            return View(group);
         }
 
+        // ============= POST MANAGEMENT ==============
         [HttpPost]
         public async Task<IActionResult> ApprovePost(int postId, int groupId)
         {
-            await _moderatorService.ApprovePost(postId, this.userId.Value);
-            return RedirectToAction("GroupManagement", new { groupId });
-        }
-        [HttpPost]
-        public async Task<IActionResult> RejectPost(int postId, int groupId)
-        {
-            await _moderatorService.RejectPost(postId, this.userId.Value);
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
+            await _moderatorService.ApprovePost(postId, userId.Value);
             return RedirectToAction("GroupManagement", new { groupId });
         }
 
         [HttpPost]
-        public async Task<IActionResult> PromoteToModerator(int userId, int groupId)
+        public async Task<IActionResult> RejectPost(int postId, int groupId)
         {
-            await _adminGroupService.AddModerator(userId, groupId);
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
+            await _moderatorService.RejectPost(postId, userId.Value);
             return RedirectToAction("GroupManagement", new { groupId });
         }
+
+        // ============= MEMBER MANAGEMENT ==============
         [HttpPost]
-        public async Task<IActionResult> DemoteToMember(int userId, int groupId)
+        public async Task<IActionResult> PromoteToModerator(int targetUserId, int groupId)
         {
-            await _adminGroupService.RemoveModerator(userId, groupId);
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
+            await _adminGroupService.AddModerator(targetUserId, groupId);
             return RedirectToAction("GroupManagement", new { groupId });
         }
+
         [HttpPost]
-        public async Task<IActionResult>BanMember(int userId, int groupId, string reason)
+        public async Task<IActionResult> DemoteToMember(int targetUserId, int groupId)
         {
-            await _moderatorService.BanUser(userId, groupId, this.userId.Value ,reason);
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
+            await _adminGroupService.RemoveModerator(targetUserId, groupId);
             return RedirectToAction("GroupManagement", new { groupId });
         }
+
         [HttpPost]
-        public async Task<IActionResult> KickMember(int userId, int groupId)
+        public async Task<IActionResult> BanMember(int targetUserId, int groupId, string reason)
         {
-            await _moderatorService.KickUser(userId, groupId, this.userId.Value);
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
+            await _moderatorService.BanUser(targetUserId, groupId, userId.Value, reason);
             return RedirectToAction("GroupManagement", new { groupId });
         }
-        //[HttpPost]
-        //public async Task<IActionResult> EditGroupInformation(int userId, int groupId)
-        //{
-        //    await _adminGroupService.UpdateGroup(userId, groupId, this.userId.Value);
-        //    return RedirectToAction("GroupManagement", new { groupId });
-        //}
+
+        [HttpPost]
+        public async Task<IActionResult> KickMember(int targetUserId, int groupId)
+        {
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
+            await _moderatorService.KickUser(targetUserId, groupId, userId.Value);
+            return RedirectToAction("GroupManagement", new { groupId });
+        }
+
+        // ============= ADMIN GROUP ==============
         [HttpPost]
         public async Task<IActionResult> ReleaseGroup(int groupId)
         {
+            if (!CheckLogin())
+                return RedirectToAction("Login", "Auth");
+
             await _adminGroupService.DeleteGroup(groupId);
             return RedirectToAction("Index");
         }
-        #endregion
     }
 }
